a89eccfc60018eacbd7480d628f30ade62cecf98
David Bruant <bruant.d@gmail.com>
2016-05-17 01:20:10 +0200

    sunburst year selection

--- js/makeYearSelector.js
+++ js/makeYearSelector.js
@@ -6,12 +6,28 @@
         var container = document.createElement('div');
         container.classList.add('years');
         
+        var selected;
+        var selectedButton;
+        
         years.forEach(function(y){
             var b = document.createElement('button');
             b.textContent = y;
 
             b.addEventListener('click', function(e){
-                onYearSelected(y);
+                if(selectedButton)
+                    selectedButton.classList.remove('selected');
+                
+                if(selected === y){
+                    selectedButton = undefined;
+                    selected = undefined;
+                }
+                else{
+                    selectedButton = b;
+                    selected = y;
+                    b.classList.add('selected');
+                }
+                
+                onYearSelected(selected);
             })
 
             container.appendChild(b);
